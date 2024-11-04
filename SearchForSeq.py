f01135f2fc5d3ec9840d51d01d989441876680db
TheEyeOfTheUniverse <105928017+noadrow@users.noreply.github.com>
2024-11-04 13:08:57 +0200

    new script - without Bio library

--- SearchForSeq.py
+++ SearchForSeq.py
@@ -1,8 +1,8 @@
-from Bio import SeqIO
-from Bio import AlignIO
-import os
+#import Bio.SeqIO as SeqIO
 import re
-import sys
+from tkinter.filedialog import askopenfilename
+from tkinter import simpledialog
+
 
 # This script search for patterns in fasta file format
 # It creates bed and fasta files for each sequence that has that pattren
@@ -10,45 +10,72 @@ import sys
 # This script takes only the file name "fileToSearch"
 # This script search and create files that located at the same folder of the script
 # This script uses regx for special nuclotide notation (such as N) you can place all nucletodies under []
+def SeqIO_parse(fileToSearch):
+    with open(fileToSearch, "r") as file:
+        seq_data = {"id":[],"seq":[]}
+        flag = False
+        seq = ""
+        for line in file:
+            line = line.strip()
+            if line.startswith(">"):
+                flag = not flag
+                seq_id = line[1:]
+                seq_data["id"].append(seq_id) # Remove '>' and take the ID
+            else:
+                seq = seq + line
+
+            if not flag:
+                seq_data["seq"].append(seq)
+                seq = ""
+                flag = not flag
+
+    return seq_data
+
 
 dfs = []
 files = []
 searchlist = []
 pattrenList = []
 #fileToSearch = "GFR_positive"
-fileToSearch = sys.argv[1]
+fileToSearch = askopenfilename(title="Select fasta")
 #pattrensToSearch = ['TGA[AG]TCA','ATTCC','CCGGAA']
-pattrensToSearch = sys.argv[2:]
+pattrensToSearch = simpledialog.askstring("enter motif sequence", "Enter a string:")
 
 def searchSeq(x):
-    for i in x:
         dfs.append(["",""])
-        files.append([open(fileToSearch+"_"+i+".fa","w"),open(fileToSearch+"_"+i+".bed","w")])
-        
-        pattrenList.append(re.compile(i))
+        files.append([open(fileToSearch+"_"+x+".fa","w"),open(fileToSearch+"_"+x+".bed","w")])
+        pattrenList.append(re.compile(x))
    
 searchSeq(pattrensToSearch)
 
-print (files)
-print (dfs)
+#save input files and data frames
+temp_df = []
+temp_df += files
+
+open("protocol_dfs_log.txt","w").write(str(files))
 
 for (file, df, pattern) in zip(files,dfs,pattrenList):
-    for seq_record in SeqIO.parse(fileToSearch+".fa", "fasta"):
-            
-        seqId = seq_record.id
-        seq = str(seq_record.seq).upper()
+    seq_record = SeqIO_parse(fileToSearch)
+    for i in range(len(seq_record["seq"])):
+        seqId = seq_record["id"][i]
+        seq = str(seq_record["seq"][i]).upper()
 
         if pattern.findall(seq):
-                    
-            start = int(seqId.split(":")[1].split("-")[0])+pattern.search(seq).start()
-            end = int(seqId.split(":")[1].split("-")[1])-len(seq)+pattern.search(seq).end()
+            df[0] += f">{seqId}\n{seq}\n"
 
-            df[0] += f"{seqId}\n{seq}\n"
-            chrx = seqId.split(":")[0]
-            df[1] += f"{chrx}\t{str(start)}\t{str(end)}\n"
+            ##UCSC seqID format
+            #chr = seqId.split("=")[1].split(":")[0]
+            #start = seqId.split("=")[1].split(":")[1].split("-")[0]
+            #end = seqId.split("=")[1].split(":")[1].split("-")[1].split(" ")[0]
+
+            ## bedtools getfasta seqID format
+            chr = seqId.split(":")[0]
+            start = seqId.split(":")[1].split("-")[0]
+            end = seqId.split(":")[1].split("-")[1]
+            df[1] += f"{chr}\t{start}\t{end}\n"
 
     file[0].write(df[0])
-    file[1].write(df[1])
     file[0].close()
-    file[1].close()
 
+    file[1].write(df[1])
+    file[1].close()
